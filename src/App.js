c42308791d0989a2a4f239b88e333dcae655cdd5
Andrew Galvez <dogisamoose.amazon@gmail.com>
2025-07-12 12:29:01 -0700

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,91 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react';
 
-function App() {
+function GameBox({ gamedata }) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className= "game-box">
+      <div className = "game-box-img">
+	<img src={gamedata.imgpath}/>
+
+    </div>
+      <div className = "game-box-desc">
+    <p>
+	  { gamedata.name }
+    </p>
+	  <a target="_blank" href={gamedata.githubpath} ><button disabled={
+	    gamedata.githubpath === "none"
+	  }>Source Code</button></a>
+	  <a download href={gamedata.downloadlinux}><button disabled={
+	    gamedata.downloadlinux === "none"}>Download for Linux</button></a>
+	  <a download href={gamedata.downloadwindows}><button disabled={
+	    gamedata.downloadwindows === "none"}>Download for Windows</button></a>
+    </div>
     </div>
   );
+};
+
+function GameBoxesWrapper({games_data}) {
+  return (
+    <ul className="game-boxes-wrapper">
+      {games_data.map((game, i) => <li key={i}><GameBox gamedata={game}/></li>)}
+    </ul>);
+};
+
+function Header({setFeedback}) {
+  return (
+    <div className="header">
+      <div className="feedback-button">
+	<button id="feedback-button" onClick={setFeedback}>Feedback</button>
+      </div>
+    </div>
+  );
+}
+
+function FeedbackPanel({runSendFeedback}) {
+  const [text, setText] = useState('');
+
+  const sendFeedback = () => {
+    runSendFeedback(text);
+  }
+
+  return (
+    <div className="feedback-panel">
+      <div className="feedback-panel-main">
+      <textarea onChange={e => setText(e.target.value)}/>
+      <button onClick={sendFeedback}>Send</button>
+    </div>
+    <div className="feedback-panel-right">
+    <h3>Feedback</h3>
+    <ul>
+    <li>Bugs</li>
+    <li>Comments</li>
+    <li>Suggestions</li>
+    </ul>
+    <p>Thanks for sending feedback!</p>
+    </div>
+    </div>
+  );
+}
+
+function App() {
+  const [games_data, setGamesData] = useState([]);
+  useEffect(() => {
+    fetch('/games.json')
+      .then(res => res.json())
+      .then(data => setGamesData(data))
+  }, []);
+
+  const [isFeedbackDisplayed, setIsFeedbackDisplayed] = useState(false);
+
+  function sendFeedback(text) {
+    console.log(text);
+    window.location.href = "mailto:dogisamoose.amazon@gmail.com?subject=Feedback&body="+text;
+  };
+
+  return (<>
+    <Header setFeedback={()=>{setIsFeedbackDisplayed(!isFeedbackDisplayed);}}/>
+    {isFeedbackDisplayed && <FeedbackPanel runSendFeedback={sendFeedback}/>}
+    <GameBoxesWrapper className="game-boxes-wrapper" games_data={games_data}/>
+    </>);
 }
 
 export default App;
