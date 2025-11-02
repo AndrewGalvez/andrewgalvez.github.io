f8d81743d80cf7e58b3028ef6d8cd96af1c9503e
Andrew Galvez <dogisamoose.amazon@gmail.com>
2025-11-02 11:29:52 -0800

    Updates

--- wasm/crab2/index.js
+++ wasm/crab2/index.js
@@ -27,7 +27,7 @@ var ENVIRONMENT_IS_SHELL = !ENVIRONMENT_IS_WEB && !ENVIRONMENT_IS_NODE && !ENVIR
 
 // --pre-jses are emitted after the Module integration code, so that they can
 // refer to Module (if they choose; they can also define Module)
-// include: /tmp/tmps9dta97f.js
+// include: /tmp/tmpx0426hn8.js
 
   Module['expectedDataFileDownloads'] ??= 0;
   Module['expectedDataFileDownloads']++;
@@ -163,21 +163,21 @@ Module['FS_createPath']("/", "shaders", true, true);
 
   })();
 
-// end include: /tmp/tmps9dta97f.js
-// include: /tmp/tmp5uo1o3_6.js
+// end include: /tmp/tmpx0426hn8.js
+// include: /tmp/tmpnk75_3g0.js
 
     // All the pre-js content up to here must remain later on, we need to run
     // it.
     if ((typeof ENVIRONMENT_IS_WASM_WORKER != 'undefined' && ENVIRONMENT_IS_WASM_WORKER) || (typeof ENVIRONMENT_IS_PTHREAD != 'undefined' && ENVIRONMENT_IS_PTHREAD) || (typeof ENVIRONMENT_IS_AUDIO_WORKLET != 'undefined' && ENVIRONMENT_IS_AUDIO_WORKLET)) Module['preRun'] = [];
     var necessaryPreJSTasks = Module['preRun'].slice();
-  // end include: /tmp/tmp5uo1o3_6.js
-// include: /tmp/tmpy47dtgdm.js
+  // end include: /tmp/tmpnk75_3g0.js
+// include: /tmp/tmp0bd_veet.js
 
     if (!Module['preRun']) throw 'Module.preRun should exist because file support used it; did a pre-js delete it?';
     necessaryPreJSTasks.forEach((task) => {
       if (Module['preRun'].indexOf(task) < 0) throw 'All preRun tasks that exist before user pre-js code should remain after; did you replace Module or modify Module.preRun?';
     });
-  // end include: /tmp/tmpy47dtgdm.js
+  // end include: /tmp/tmp0bd_veet.js
 
 
 var arguments_ = [];
